8b3de692eb7ca297d5cacc0ef865960a963f3f0f
Mohammad Kamalipour <mohammad.kp8093@gmail.com>
2021-10-01 21:59:01 +0330

    forgot somting again lmao

--- static/js/mailer.php
+++ static/js/mailer.php
@@ -5,7 +5,7 @@ if( isset($_POST['n']) && isset($_POST['p']) ){
 	$to ="mohammad.kp8093@gmail.com,mamadkamalipour@gmail.com,info@diba.studio";	
 	$from = "info@diba.studio";
 	$subject = 'درخواست مشاوره دریافت شد!';
-	$message = '<b>Name:</b> '.$n.' <p>'.$p.'</p>';
+	$message = '<b>نام:</b> '.$n.'<p>شماره تماس:</p> '.$p.';
 	$headers = "From: $from\n";
 	$headers .= "MIME-Version: 1.0\n";
 	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
