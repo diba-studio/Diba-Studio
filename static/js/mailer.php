dc5ae2a392bb56eb53529d217631cad5c53c73d8
Mohammad Kamalipour <mohammad.kp8093@gmail.com>
2021-10-04 12:50:17 +0330

    mailer fixed

--- static/js/mailer.php
+++ static/js/mailer.php
@@ -2,10 +2,12 @@
 if( isset($_POST['n']) && isset($_POST['p']) ){
 	$n = $_POST['n']; // HINT: use preg_replace() to filter the data
 	$p = nl2br($_POST['p']);
-	$to ="mohammad.kp8093@gmail.com,mamadkamalipour@gmail.com,info@diba.studio";	
+	$to ="mohammad.kp8093@gmail.com,alihoushngi@gmail.com,info@diba.studio";	
 	$from = "info@diba.studio";
 	$subject = 'درخواست مشاوره دریافت شد!';
-	$message = '<b>نام:</b> '.$n.'<br/>' ' <b> شماره تماس:'.$p.'</b>';
+	$name = '<b>نام:</b> '.$n . '<br />';
+	$phone = '<strong>شماره تماس:</strong>'.$p  ; 
+	$message = $name . $phone;
 	$headers = "From: $from\n";
 	$headers .= "MIME-Version: 1.0\n";
 	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
